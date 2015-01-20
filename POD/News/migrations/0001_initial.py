e18bb38ce62edbe165f0b35a086a1eae85f364cb
Vlad171 <vladimirzyuzin@inbox.ru>
2015-01-20 23:31:07 +0400

    Изменение моделиЮ

---
+++ POD/News/migrations/0001_initial.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.files.storage
+import datetime
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Event',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('titleEvent', models.CharField(max_length=200)),
+                ('bodyEvent', models.TextField()),
+                ('slug', models.SlugField(unique=True)),
+                ('publish', models.DateTimeField(default=datetime.datetime.now)),
+                ('created', models.DateTimeField(auto_now=True)),
+                ('modified', models.DateTimeField(auto_now_add=True)),
+                ('author', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Photo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('photo', models.ImageField(storage=django.core.files.storage.FileSystemStorage(location=b'/media/photos'), upload_to=b'')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='eventPhoto',
+            field=models.ForeignKey(blank=True, to='News.Photo', null=True),
+            preserve_default=True,
+        ),
+    ]
--- POD/News/models.py
+++ POD/News/models.py
@@ -1,27 +1,31 @@
 from django.db import models
+from django.db.models import permalink
+import datetime
 from django.core.files.storage import FileSystemStorage
+from django.contrib import admin
 from django.contrib.auth.models import User
 # Create your models here.
 
 fs=FileSystemStorage(location='/media/photos')
 
 class Event(models.Model):
-	titleEvent=models.CharField(max_length=200)
-	bodyEvent=models.TextField()
-	author = models.ForeignKey(User, blank=True, null=True)
-	slug = models.SlugField(unique=True)
-	publish = models.DateTimeField(default=datetime.datetime.now)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-    eventPhoto=ForeignKey('Photo', blank=True,null=True)
+    titleEvent=models.CharField(max_length=200)
+    bodyEvent=models.TextField()
+    author = models.ForeignKey(User, blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    publish = models.DateTimeField(default=datetime.datetime.now)
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now_add=True)
+    eventPhoto=models.ForeignKey('Photo', blank=True,null=True)
 
     def __unicode__(self):
-        return u'%s' % self.title
+        return u'%s' % self.titleEvent
+    @permalink
+    def get_absolute_url(self):
+    	return ('django.views.generic.list_detail.object_detail', None, {'object_id':self.id})
 
-class Photo(model.Model):
+class Photo(models.Model):
     photo= models.ImageField(storage=fs)
 
-    def __unicode__(self):
-        return u'%s' % self.title
-
-
+admin.site.register(Event)
+admin.site.register(Photo)
